b8078e2470e64c4dcd669b1008334af2e099b959
trallspnw <116036130+trallspnw@users.noreply.github.com>
2024-07-16 15:25:40 -0700

    Update coinbase.py

--- python/ccxt/coinbase.py
+++ python/ccxt/coinbase.py
@@ -344,6 +344,7 @@ class coinbase(Exchange, ImplicitAPI):
                 '30m': 'THIRTY_MINUTE',
                 '1h': 'ONE_HOUR',
                 '2h': 'TWO_HOUR',
+                '4h': 'FOUR_HOUR',
                 '6h': 'SIX_HOUR',
                 '1d': 'ONE_DAY',
             },
